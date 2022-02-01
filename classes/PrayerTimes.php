18b4cda4e540af12e28027a81d07a6b67526a423
Abdoulaziz <yaronfada@gmail.com>
2022-02-01 18:26:46 +0100

    Ajout de la classe active et reajustement des horaires de prière à venir

--- classes/PrayerTimes.php
+++ classes/PrayerTimes.php
@@ -2,59 +2,67 @@
 
 class PrayerTimes {
 
+    public $azan_name;
+    public $iqamah_name;
+    public $masjid_name;
+    public $current_time;
+    public $next_prayer;
+    public $site_link;
+
     public function __construct() {
 
-      $time = time();
 
-      $pt = new PrayTime();  
+        $this->azan_name =  ['en' => 'Azan','ar' => 'الأذان'];
+        $this->iqamah_name = ['en' => 'Iqamah','ar' => 'الإقامة'];
+        $this->masjid_name = ['en' => 'Mosquee Dhoun Nourain','ar' => 'مسجد ذو النورين'];
 
-      $nex_prayer = $this->get_next_prayer_time();
+        // set default timezone
+        date_default_timezone_set('America/Montreal');
       
-            
-      $Arabic = new I18N_Arabic('Date');
+        // get current time
+        $this->current_time = time();
+        
+        // initialize today prayer and iqamah times
+        $this->fajr_iqamah_minutes = 15;
+        $this->zuhr_iqamah_minutes = 10;
+        $this->asr_iqamah_minutes = 10;
+        $this->maghrib_iqamah_minutes = 5;
+        $this->isha_iqamah_minutes = 15;
+
+        // get current hijr date
+        $this->current_hijr_date = $this->get_hijr_date($this->current_time);
+        $this->today_prayer_times = $this->get_today_prayer_times();
+        $this->next_prayer = $this->get_next_prayer_time();
+
+        // this site link
+        $this->site_link = 'www.salafidemontreal.com';
+    }
+
+    // get current hijr date
+    public function get_hijr_date($time){
+        
+      $Arabic = new I18N_Arabic_Date(); // create new instance of I18N_Arabic_Date class
       // $Arabic->setMode(1);
       $correction = $Arabic->dateCorrection ($time);    
         
-      $current_hijr_date = $Arabic->date('dS', $time, $correction).' '.$Arabic->date('F', $time, $correction).', '.$Arabic->date('Y', $time, $correction);
-      
-      $azan_name = [
-        'en' => 'Azan',
-        'ar' => 'الأذان'
-      ];
-      $iqamah_name = [
-        'en' => 'Iqamah',
-        'ar' => 'الإقامة'
-      ];
-      $majid_name = [
-        'en' => 'Mosquee Dhoun Nourain',
-        'ar' => 'مسجد ذو النورين'
-      ];
-      
-  
-      $time_array = getdate($time);
-      
-      // get current time
-      $this->time = $time; 
-  
-      $this->azan_name = $azan_name['en'] . ' (' . $azan_name['ar'] . ')';
-      $this->iqamah_name = $iqamah_name['en'] . ' (' . $iqamah_name['ar'] . ')';
-      $this->majid_name = $majid_name['en'] . ' ' . $majid_name['ar'];
+      $current_hijr_date = $Arabic->date('dS',  $this->time, $correction).' '.$Arabic->date('F',  $time, $correction).', '.$Arabic->date('Y', $time, $correction);
 
-      // initialize today prayer and iqamah times
+      return $current_hijr_date;
+    }
 
-      $this->fajr_iqamah_minutes = 15;
-      $this->dhuhr_iqamah_minutes = 10;
-      $this->asr_iqamah_minutes = 10;
-      $this->maghrib_iqamah_minutes = 5;
-      $this->isha_iqamah_minutes = 15;
+    // get azan name
+    public function get_azan_name() {
+        return $this->azan_name['en'] . ' (' . $this->azan_name['ar'] . ')';
+    }
 
-      // get current hijr date
-      $this->current_hijr_date = $current_hijr_date;
-      $this->today_prayer_times = $this->get_today_prayer_times();
-      $this->nex_prayer = $nex_prayer;
+    // get iqamah name
+    public function get_iqamah_name() {
+        return $this->iqamah_name['en'] . ' (' . $this->iqamah_name['ar'] . ')';
+    }
 
-      // this site link
-      $this->site_link = 'www.salafidemontreal.com';
+    // get majid name
+    public function get_masjid_name() {
+        return $this->masjid_name['en'] . ' (' . $this->masjid_name['ar'] . ')';
     }
   
     public function get_months($month_number){
@@ -67,16 +75,16 @@ class PrayerTimes {
           $month_name = ['ar' => 'فبراير', 'fr' => 'Février'];
           break;
         case 3:
-          $month_name = [ 'ar' => 'مارس', 'fr' => 'Mars'];
+          $month_name = ['ar' => 'مارس', 'fr' => 'Mars'];
           break;
         case 4:
-          $month_name = [ 'ar' => 'أبريل', 'fr' => 'Avril'];
+          $month_name = ['ar' => 'أبريل', 'fr' => 'Avril'];
           break;
         case 5:
-          $month_name = [ 'ar' => 'مايو', 'fr' => 'Mai'];
+          $month_name = ['ar' => 'مايو', 'fr' => 'Mai'];
           break;
         case 6:
-          $month_name = [  'ar' => 'يونيو', 'fr' => 'Juin'];
+          $month_name = [ 'ar' => 'يونيو', 'fr' => 'Juin'];
           break;
         case 7:
           $month_name = ['ar' => 'يوليو', 'fr' => 'Juillet'];
@@ -116,8 +124,7 @@ class PrayerTimes {
    
     // get the next prayer time depending on the current time
     public function get_next_prayer_time(){
-      $time = time();
-      $current_time = date('H:i', $time);
+      $current_time = date('H:i', $this->current_time);
       $time = strtotime($current_time);
       $today_prayer_times = $this->get_today_prayer_times();
       $next_prayer_time = '';
@@ -125,14 +132,14 @@ class PrayerTimes {
 
       foreach ($today_prayer_times as $prayer_name => $prayer_time) {
         $prayer_time = strtotime($prayer_time);
-        if ($time < $prayer_time) {
+        if ($time < $prayer_time && $prayer_name != 'sunrise' && $prayer_name != 'sunset') {
           $next_prayer_time = $prayer_time;
           $next_prayer_name = $prayer_name;
           break;
         }else{
-          // set next prayer time to fajr
-          $next_prayer_time = strtotime($today_prayer_times['fajr']);
-          $next_prayer_name = 'fajr';
+          // set next prayer time to next day fajr
+        $next_prayer_time = strtotime($this->get_next_day_prayer_times()['fajr']);
+        $next_prayer_name = 'fajr';
     
         }
       }
@@ -154,7 +161,7 @@ class PrayerTimes {
     // get today prayers times
     public function get_today_prayer_times(){
       $pt = new PrayTime();   
-      $timestamp = time();
+      $timestamp = $this->current_time;
       $year = date('Y', $timestamp);
       $month = date('m', $timestamp);
       $day = date('d', $timestamp);
@@ -169,7 +176,7 @@ class PrayerTimes {
       $today_prayer_times = array(
         'fajr' => $today_prayer_times[0],
         'sunrise' => $today_prayer_times[1],
-        'dhuhr' => $today_prayer_times[2],
+        'zuhr' => $today_prayer_times[2],
         'asr' => $today_prayer_times[3],
         'sunset' => $today_prayer_times[4],
         'maghrib' => $today_prayer_times[5],
@@ -178,6 +185,35 @@ class PrayerTimes {
       return $today_prayer_times;
 
     }
+
+     // get next day prayer times
+     public function get_next_day_prayer_times(){
+      $pt = new PrayTime();   
+      // get next day prayer times
+      $timestamp = strtotime('+1 day', $this->current_time);
+      $year = date('Y', $timestamp);
+      $month = date('m', $timestamp);
+      $day = date('d', $timestamp);
+      $latidude_montreal = 45.5017;
+      $longitude_montreal = -73.5673;
+      $timezone_montreal = -5;
+
+      $next_day_prayer_times = $pt->getDatePrayerTimes($year, $month, $day, $latidude_montreal, $longitude_montreal, $timezone_montreal);
+
+      // changeto the today_prayer_times array keys to match the prayer_times array keys
+      $next_day_prayer_times = array(
+        'fajr' => $next_day_prayer_times[0],
+        'sunrise' => $next_day_prayer_times[1],
+        'zuhr' => $next_day_prayer_times[2],
+        'asr' => $next_day_prayer_times[3],
+        'sunset' => $next_day_prayer_times[4],
+        'maghrib' => $next_day_prayer_times[5],
+        'isha' => $next_day_prayer_times[6]
+      );
+
+      return $next_day_prayer_times;
+    }
+
 
      // get iqaamah time
      public function get_iqamah_time($prayer_name){
@@ -201,8 +237,8 @@ class PrayerTimes {
     public function get_prayer_name_ar($prayer_name){
       $prayer_name_ar = [
         'fajr' => 'الفجر',
-        'shuruk' => 'الشروق',
-        'dhuhr' => 'الظهر',
+        'shurooq' => 'الشروق',
+        'zuhr' => 'الظهر',
         'asr' => 'العصر',
         'maghrib' => 'المغرب',
         'isha' => 'العشاء'
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 setlocale(LC_TIME, 'fr_FR.utf8','fra'); 
 
-$classes = ['PrayerTimes', 'Arabic', 'prayTimes'];
+$classes = ['PrayerTimes', 'prayTimes'];
 
 // include classes
 foreach ($classes as $class) {
@@ -14,20 +14,20 @@ require_once 'Date.php';
 </style>
 <table class="prayer_time">
   <?php $prayertimes = new PrayerTimes(); ?>
-  <?php $next_prayer_name = strtolower($prayertimes->nex_prayer['name']); ?>
+  <?php $next_prayer_name = strtolower($prayertimes->next_prayer['name']); ?>
     <tr>
      <td class="flip-card-outer" colspan="4">
           <div class="flip-card">
               <div class="flip-card-inner">
                   <div class="flip-card-front">
-                      <span class="arabic_text"><?php echo $prayertimes->majid_name; ?></span><br>
-                      <div class="time_now"><span><?php echo date('h:i', $prayertimes->time) . ' ' ?></span><?php echo date('A', $prayertimes->time); ?></div>
-                      <span class="date"><?php echo ucfirst(strftime("%A")) . ' ' . date('d', $prayertimes->time) . ' ' . $prayertimes->get_months(date('m', $prayertimes->time))['fr']; ?>  <span class="arabic_text"> <?php echo '('.$prayertimes->get_months(date('m', $prayertimes->time))['ar'].') '; ?></span><?php echo date('Y', $prayertimes->time); ?></span><br>
+                      <span class="arabic_text"><?php echo $prayertimes->get_masjid_name(); ?></span><br>
+                      <div class="time_now"><span><?php echo date('h:i', $prayertimes->current_time) . ' ' ?></span><?php echo date('A', $prayertimes->current_time); ?></div>
+                      <span class="date"><?php echo ucfirst(strftime("%A")) . ' ' . date('d', $prayertimes->current_time) . ' ' . $prayertimes->get_months(date('m', $prayertimes->current_time))['fr']; ?>  <span class="arabic_text"> <?php echo '('.$prayertimes->get_months(date('m', $prayertimes->current_time))['ar'].') '; ?></span><?php echo date('Y', $prayertimes->current_time); ?></span><br>
                       <span class="date"><?php echo $prayertimes->current_hijr_date; ?></span>
                   </div>
                   <div class="flip-card-back">
-                      <span class="arabic_text"><?php echo $prayertimes->majid_name; ?></span><br>
-                      <div class="time_now_text"><span>iqama temps de prière zuhr</div><br>
+                      <span class="arabic_text"><?php echo $prayertimes->get_masjid_name(); ?></span><br>
+                      <div class="time_now_text"><span>Iqama temps de prière  <?php echo ucfirst($next_prayer_name); ?></span></div>
                       <span class="date"><?php echo date('d', $prayertimes->time) . ' ' . $prayertimes->get_months(date('m', $prayertimes->time))['fr']; ?>  <span class="arabic_text"> <?php echo '('.$prayertimes->get_months(date('m', $prayertimes->time))['ar'].'), '; ?></span><?php echo date('Y', $prayertimes->time); ?></span><br>
                           <span class="date"><?php echo $prayertimes->current_hijr_date; ?></span>
                       </div>
@@ -41,16 +41,16 @@ require_once 'Date.php';
             <div class="flip-card-front">
 
                 <span class="arabic_text">Copyright &#169; 2022 Mosque prayer Times All rights reserved<br><br>
-                <span class="time_now_right"><?php echo $prayertimes->nex_prayer['name'] . ' ('.$prayertimes->get_prayer_name_ar($next_prayer_name).')'; ?></span><br>
+                <span class="time_now_right"><?php echo $prayertimes->next_prayer['name'] . ' ('.$prayertimes->get_prayer_name_ar($next_prayer_name).')'; ?></span><br>
                 <table class="prayer_time_now">
                     <tr>
-                        <td class="prayer_time_now_right_label"><?php echo $prayertimes->azan_name; ?></td>
-                        <td class="prayer_time_now_right"><?php echo $prayertimes->nex_prayer['time']; ?></td>
+                        <td class="prayer_time_now_right_label"><?php echo $prayertimes->get_azan_name(); ?></td>
+                        <td class="prayer_time_now_right"><?php echo $prayertimes->next_prayer['time']; ?></td>
                     </tr>
 
                     <tr>
-                        <td class="prayer_time_now_right_label"><?php echo $prayertimes->iqamah_name; ?></td>
-                        <td class="prayer_time_now_right"><?php echo $prayertimes->get_iqamah_time($next_prayer_name); ?></td>
+                        <td class="prayer_time_now_right_label"><?php echo $prayertimes->get_iqamah_name(); ?></td>
+                        <td class="prayer_time_now_right"><?php echo $prayertimes->next_prayer['iqamah']; ?></td>
                     </tr>
 
                 </table>
@@ -58,7 +58,7 @@ require_once 'Date.php';
             </div>
             <div class="flip-card-back">
                 <span class="arabic_text">Copyright &#169; 2022 Mosque prayer Times All rights reserved<br><br>
-                <span class="time_now_right"><?php echo $prayertimes->nex_prayer['name'] . ' ('.$prayertimes->get_prayer_name_ar($next_prayer_name).')'; ?></span><br>
+                <span class="time_now_right"><?php echo $prayertimes->next_prayer['name'] . ' ('.$prayertimes->get_prayer_name_ar($next_prayer_name).')'; ?></span><br>
                 <div class="time_now_text"><span>وقت إقامة الصلاة <?php echo $prayertimes->get_prayer_name_ar($next_prayer_name); ?></div><br>
                       <span class="link"><?php echo $prayertimes->site_link; ?></span>
                 </div>
@@ -81,22 +81,69 @@ require_once 'Date.php';
             <td class="bottom_label bottom_label_time">Azan <span class="arabic_text">(الأذان)</span></td>
             <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_prayer_time('fajr'); ?></td>
             <td class="bottom_label bottom_label_time" rowspan="2"><?php echo $prayertimes->get_prayer_time('sunrise'); ?></td>
-            <td class="bottom_label bottom_label_time bottom_label_time_active"><?php echo $prayertimes->get_prayer_time('dhuhr'); ?></td>
+            <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_prayer_time('zuhr'); ?></td>
             <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_prayer_time('asr'); ?></td>
             <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_prayer_time('maghrib'); ?></td>
             <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_prayer_time('isha'); ?></td>
             <td class="bottom_label bottom_label_time" rowspan="2">
-                <span class="small_text_one">Khutbah <span class="arabic_text">(الخطبة)</span></span><br> <?php echo $prayertimes->get_prayer_time('dhuhr'); ?>
+                <span class="small_text_one">Khutbah <span class="arabic_text">(الخطبة)</span></span><br> <?php echo $prayertimes->get_prayer_time('zuhr'); ?>
             </td>
         </tr>
         <tr>
-            <td class="bottom_label bottom_label_time">Iqama <span class="arabic_text">(الإقامة)</span></td>
-            <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_iqamah_time('fajr'); ?></td>
-            <td class="bottom_label bottom_label_time bottom_label_time_active"><?php echo $prayertimes->get_iqamah_time('dhuhr'); ?></td>
-            <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_iqamah_time('asr'); ?></td>
-            <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_iqamah_time('maghrib'); ?></td>
-            <td class="bottom_label bottom_label_time"><?php echo $prayertimes->get_iqamah_time('isha'); ?></td>
+            <td class="bottom_label bottom_label_time iqamah">Iqama <span class="arabic text">(الإقامة)</span></td>
+            <td class="bottom_label bottom_label_time iqamah"><?php echo $prayertimes->get_iqamah_time('fajr'); ?></td>
+            <td class="bottom_label bottom_label_time iqamah"><?php echo $prayertimes->get_iqamah_time('zuhr'); ?></td>
+            <td class="bottom_label bottom_label_time iqamah"><?php echo $prayertimes->get_iqamah_time('asr'); ?></td>
+            <td class="bottom_label bottom_label_time iqamah"><?php echo $prayertimes->get_iqamah_time('maghrib'); ?></td>
+            <td class="bottom_label bottom_label_time iqamah"><?php echo $prayertimes->get_iqamah_time('isha'); ?></td>
         </tr>
         
 </table>
 
+<script>
+   // add active class to current prayer
+    var current_prayer_time = '<?php echo $prayertimes->next_prayer['time']; ?>';
+    var current_prayer_iqamah = '<?php echo $prayertimes->next_prayer['iqamah']; ?>';
+
+    // get value of each bottom_label_time class with javascript
+    var bottom_label_time = document.getElementsByClassName('bottom_label_time');
+
+    // loop through each bottom_label_time class
+    for (var i = 0; i < bottom_label_time.length; i++) {
+        // if current prayer time is equal to bottom_label_time class value
+        if (current_prayer_time == bottom_label_time[i].innerHTML) {
+            // add active class to bottom_label_time class
+            bottom_label_time[i].classList.add('bottom_label_time_active');
+        }
+    }
+    
+    // add active class the iqama time
+    var bottom_label_time_iqamah = document.getElementsByClassName('iqamah');
+
+    // loop through each bottom_label_time_iqamah class
+    for (var i = 0; i < bottom_label_time_iqamah.length; i++) {
+        // if current prayer iqamah is equal to bottom_label_time_iqamah class value
+        if (current_prayer_iqamah == bottom_label_time_iqamah[i].innerHTML) {
+            // add active class to bottom_label_time_iqamah class
+            bottom_label_time_iqamah[i].classList.add('bottom_label_time_active');
+        }
+    }
+
+    // var bottom_label_time = $('.bottom_label_time').val();
+    // // doc ready
+    // $(document).ready(function(){
+    //     // loop through each bottom_label_time class
+    //     bottom_label_time.each(function(){
+    //         // get value of each bottom_label_time class
+    //         var bottom_label_time_value = $(this).text();
+    //         // if current prayer time is equal to bottom_label_time value
+    //         if(current_prayer_name == bottom_label_time_value){
+    //             // add class active
+    //             $(this).addClass('bottom_label_time_active');
+    //         }
+    //     });
+    // });
+
+   
+   
+</script>
