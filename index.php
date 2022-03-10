5242194932a7d610ef504631640243628a867580
Abdoulaziz <yaronfada@gmail.com>
2022-03-10 14:57:38 +0100

    Ajout du Live heure:minites:secondes pour l\heure actuelle

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once 'Date.php';
 <style>
 <?php include 'css/style.css'; ?>
 </style>
+<body  onload=dispaly_live_time();>
 <table class="prayer_time">
   <?php $prayertimes = new PrayerTimes(); ?>
   <?php $current_prayer_name = strtolower($prayertimes->current_prayer['name']); ?>
@@ -19,11 +20,12 @@ require_once 'Date.php';
      <td class="flip-card-outer" colspan="4">
           <div class="flip-card">
               <div class="flip-card-inner">
-                  <div class="flip-card-front">
+                  <div class="flip-card-front" id="flip-card-front-top-left">
                       <span class="arabic_text"><?php echo $prayertimes->get_masjid_name(); ?></span><br>
-                      <div class="time_now"><span><?php echo date('h:i', $prayertimes->current_time) . ' ' ?></span><?php echo date('A', $prayertimes->current_time); ?></div>
+                      <div id="time_now_live" class="time_now"><span><?php echo date('h:i', $prayertimes->current_time) . ' ' ?></span><?php echo date('A', $prayertimes->current_time); ?></div>
                       <input type="hidden" id="current_hour" value="<?php echo date('H', $prayertimes->current_time); ?>">
                       <input type="hidden" id="current_minute" value="<?php echo date('i', $prayertimes->current_time); ?>">
+                      <span ></span>
                       <span class="date"><?php echo ucfirst(strftime("%A")) . ' ' . date('d', $prayertimes->current_time) . ' ' . $prayertimes->get_months(date('m', $prayertimes->current_time))['fr']; ?>  <span class="arabic_text"> <?php echo '('.$prayertimes->get_months(date('m', $prayertimes->current_time))['ar'].') '; ?></span><?php echo date('Y', $prayertimes->current_time); ?></span><br>
                       <span class="date"><?php echo $prayertimes->current_hijr_date; ?></span>
                   </div>
@@ -102,16 +104,9 @@ require_once 'Date.php';
         </tr>
         
 </table>
-
+</body>
 <script type="text/javascript">
-    // var flip_card = document.getElementsByClassName('flip-card');
-    // var i;
-
-    // for (i = 0; i < flip_card.length; i++) {
-    //     flip_card[i].addEventListener('click', function() {
-    //         this.classList.toggle('flip');
-    //     });
-    // }
+   
    // add active class to current prayer
     var current_prayer_time = '<?php echo $prayertimes->next_prayer['time']; ?>';
     var current_prayer_iqamah = '<?php echo $prayertimes->next_prayer['iqamah']; ?>';
@@ -142,13 +137,39 @@ require_once 'Date.php';
 
   
     // Refresh page every 30 seconds
-    setInterval(function(){
-        // refresh page
-        window.location.reload();
-    }, 30000);
+    // setInterval(function(){
+    //     // refresh page
+    //     window.location.reload();
+    // }, 30000);
 
 
+function refresh(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('dispaly_live_time()',refresh)
+}
 
-   
-   
+function dispaly_live_time() {
+
+// get current Montreal time 
+var d = new Date();
+var n = d.toLocaleString('en-US', { timeZone: 'America/Montreal' });
+var x = new Date(n);
+var x1 = x.toLocaleString();
+
+// get hour, minute, second
+var hours = x.getHours();
+var minutes = x.getMinutes();
+var seconds = x.getSeconds();
+
+// set AM or PM
+var ampm = hours >= 12 ? 'PM' : 'AM';
+
+// set x 
+var x = hours + '<span class="blinking">:</span>' + minutes + '<span class="blinking">:</span>' + seconds;
+
+// apply x text style
+document.getElementById('time_now_live').innerHTML = '<span class="time_now"">' + x + '</span>' + ' ' + ampm;
+refresh();
+ }
+
 </script>
